a66e0627a5458e7e97977b8c30124ec0f446c4ea
Somtoo <chukwurahb@gmail.com>
2020-01-30 21:18:06 -0600

    finished partials

---
+++ web-server/src/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const app = express();
+
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname,'../public');
+const viewsPath = path.join(__dirname, '../template/views');
+const partialsPath = path.join(__dirname,'../template/partials');
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Setup static directory tp serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('',(req,res)=>{
+   res.render('index',{
+       title:'Weather App',
+       name:'Somtoo'
+   })
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About Page',
+        name:'Somtoo'
+    })
+});
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'Help Page',
+        help:'Hello you need help!!',
+        name:'Chukwurah Somtoo'
+    })
+});
+
+app.get('/weather',(req,res)=>{
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
+});
+
+app.get('*', (req,res)=>{
+
+});
+app.listen(3000, ()=>{
+    console.log('Server is up on port 3000')
+});
