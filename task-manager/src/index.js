972b99921de91dafaea28c3f29e94c2c43de1b43
Somtoo <you@example.com>
2020-05-19 12:02:06 -0500

    Completed async-await.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -12,6 +12,7 @@ app.post('/users',(req,res)=>{
     const user = new User(req.body);
     user.save().then(()=>{
         res.send(user)
+
     }).catch((error)=>{
         res.status(400).send(error)
 
