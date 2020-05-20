134e4c4f2b69fb84e50d8ef6f1cb428c29de88ef
Somtoo <you@example.com>
2020-05-20 14:58:09 -0500

    Start encryption

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,43 +1,45 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const User = mongoose.model('User',{
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required:true,
-        trim:true
+        required: true,
+        trim: true
     },
     email: {
         type: String,
         required: true,
-        trim:true,
-        lowercase:true,
+        trim: true,
+        lowercase: true,
         validate(value) {
             if (!validator.isEmail(value)) {
                 throw new Error('Email is invalid')
             }
         }
     },
-    password:{
-        type:String,
-        required:true,
-        minlength:7,
-        trim:true,
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
         validate(value) {
-            if (value.toLowerCase().includes('password')){
+            if (value.toLowerCase().includes('password')) {
                 throw new Error('Password cannot contain password')
             }
         }
     },
-    age:{
+    age: {
         type: Number,
-        default:0,
-        validate(value){
-            if (value < 0){
+        default: 0,
+        validate(value) {
+            if (value < 0) {
                 throw new Error('Age must be a positive number')
             }
         }
 
     }
-});
+})
+
+const User = mongoose.model('User',userSchema);
 
 module.exports = User;
