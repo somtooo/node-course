3e6bd1c1983738e7c3976352a64b760c38981062
Somtoo <you@example.com>
2020-05-20 14:23:19 -0500

    Completed seperation of routes

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,124 +1,13 @@
 const express = require('express');
 require('./db/mongoose');
-const User = require('./models/user');
 const Task = require('./models/task');
-
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.post('/users', async (req,res)=>{
-    const user = new User(req.body);
-    try {
-        await  user.save()
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-
-});
-
-app.get('/users',async (req,res)=>{
-
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-
-});
-
-app.get('/users/:id',async (req,res)=>{
-    const _id = req.params.id;
-    try {
-
-        const user = await User.findById(_id)
-        if (!user){
-            return res.status(404).send()
-        }
-        res.send(user)
-
-    } catch (e) {
-        res.status(500).send(e)
-    }
-});
-
-app.patch('/users/:id', async (req,res)=>{
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name','email','password','age']
-    const isValidOperation = updates.every((updates)=> allowedUpdates.includes(updates))
-    const _id = req.params.id
-    if (!isValidOperation){
-        return res.status(400).send({error:'Invalid updates!'})
-    }
-    try {
-        const user = await User.findByIdAndUpdate(_id,req.body,{new:true, runValidators: true})
-        if (!user){
-            return res.status(404).send()
-        }
-        res.send(user)
-    }catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.post('/tasks',async (req,res)=>{
-   const task = new Task(req.body);
-   try {
-       await task.save()
-       res.status(201).send(task)
-   } catch (e) {
-       res.status(400).send(e)
-   }
-});
-
-app.get('/tasks',async (req,res)=>{
-
-    try {
-        const tasks = await Task.find({})
-        res.send(tasks)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-
-});
-
-app.get('/tasks/:id',async (req,res)=>{
-   const _id = req.params.id;
-
-   try {
-       const task = await Task.findById(_id)
-       if (!task){
-           res.status(404).send(task)
-       }
-       res.send(task)
-
-   } catch (e) {
-       res.status(500).send(e)
-   }
-
-});
-
-app.patch('/users/:id', async (req,res)=>{
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name','email','password','age']
-    const isValidOperation = updates.every((updates)=> allowedUpdates.includes(updates))
-    const _id = req.params.id
-    if (!isValidOperation){
-        return res.status(400).send({error:'Invalid updates!'})
-    }
-    try {
-        const user = await User.findByIdAndUpdate(_id,req.body,{new:true, runValidators: true})
-        if (!user){
-            return res.status(404).send()
-        }
-        res.send(user)
-    }catch (e) {
-        res.status(400).send(e)
-    }
-})
+app.use(userRouter,taskRouter)
 
 app.listen(port,()=>{
     console.log('Server is up on port' + port)
